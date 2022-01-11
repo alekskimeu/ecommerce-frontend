9e31320636f42c37a5941b9521829eadcd2a92e6
alekskimeu <kim3ual3x@gmail.com>
2022-01-11 11:42:11 +0300

    Redesign Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { HeroItem } from "..";
+import { Link } from "react-router-dom";
+import ArrowForwardOutlinedIcon from "@material-ui/icons/ArrowForwardOutlined";
+import image from "../../assets/subscribe.png";
 
-import { displayProducts } from "../../data";
 import "./Hero.css";
 
 const Hero = () => {
 	return (
 		<div className="hero">
-			<h3 className="heading">Noelas Shop</h3>
 			<div className="hero-wrapper">
-				{displayProducts.map((product) => (
-					<HeroItem key={product.id} product={product} />
-				))}
+				<div className="hero-content">
+					<h1 className="content-heading">The world</h1>
+					<p className="description">
+						The world breaks everyone, and afterwards, many are strong at the
+						broken places
+					</p>
+					<div className="hero-cta">
+						<Link to="/shop">
+							Shop Now <ArrowForwardOutlinedIcon />{" "}
+						</Link>
+					</div>
+				</div>
+				<div className="hero-image">
+					<img src={image} alt="" width="300" height="300" />
+				</div>
 			</div>
 		</div>
 	);
